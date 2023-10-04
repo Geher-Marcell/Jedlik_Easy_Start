9ba2da9fa505507514698ab81340a54245027249
Geher-Marcell <darkiex03@gmail.com>
2023-10-04 15:33:09 +0200

    Első commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region MouseHook
+        [Flags]
+        public enum SPIF
+        {
+            None = 0x00,
+            /// <summary>Writes the new system-wide parameter setting to the user profile.</summary>
+            SPIF_UPDATEINIFILE = 0x01,
+            /// <summary>Broadcasts the WM_SETTINGCHANGE message after updating the user profile.</summary>
+            SPIF_SENDCHANGE = 0x02,
+            /// <summary>Same as SPIF_SENDCHANGE.</summary>
+            SPIF_SENDWININICHANGE = 0x02
+        }
+
+        // http://stackoverflow.com/questions/24737775/toggle-enhance-pointer-precision
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo", SetLastError = true)]
+        public static extern bool SystemParametersInfoGet(uint action, uint param, IntPtr vparam, SPIF fWinIni);
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo", SetLastError = true)]
+        public static extern bool SystemParametersInfoSet(uint action, uint param, IntPtr vparam, SPIF fWinIni);
+
+        public const UInt32 SPI_GETMOUSE = 0x0003;
+        public const UInt32 SPI_SETMOUSE = 0x0004;
+
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Setup();
+        }
+
+        public static bool ToggleEnhancePointerPrecision(bool b)
+        {
+            int[] mouseParams = new int[3];
+            // Get the current values.
+            SystemParametersInfoGet(SPI_GETMOUSE, 0, GCHandle.Alloc(mouseParams, GCHandleType.Pinned).AddrOfPinnedObject(), 0);
+            // Modify the acceleration value as directed.
+            mouseParams[2] = b ? 1 : 0;
+            // Update the system setting.
+            return SystemParametersInfoSet(SPI_SETMOUSE, 0, GCHandle.Alloc(mouseParams, GCHandleType.Pinned).AddrOfPinnedObject(), SPIF.SPIF_SENDCHANGE);
+        }
+
+        public static bool GetEnhancePointerPrecision()
+        {
+            int[] mouseParams = new int[3];
+            SystemParametersInfoGet(SPI_GETMOUSE, 0, GCHandle.Alloc(mouseParams, GCHandleType.Pinned).AddrOfPinnedObject(), 0);
+            return mouseParams[2] == 1;
+        }
+
+        private void Setup()
+        {
+           ((CheckBox)FindName("cb_MouseAccel")).IsChecked = GetEnhancePointerPrecision();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox box = (CheckBox) sender;
+
+            bool b; 
+            if(box.IsChecked == null) 
+            {
+                b = false;
+            }
+            else
+            {
+                b = (bool)box.IsChecked;
+            }
+
+            ToggleEnhancePointerPrecision(b);
+        }   
+    }
+}
